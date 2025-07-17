938095424b42394e2bef2641bfc1aa818fc2a62f
bammynithirathaya@gmail.com <bammynithirathaya@gmail.com>
2025-07-18 02:16:49 +0700

    save storymode in progress(not complete yet)

--- src/game/utils/api.js
+++ src/game/utils/api.js
@@ -1,7 +1,5 @@
 // src/utils/api.js
 
-const BASE_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000';
-
 export async function saveProgress(userId, scene) {
   if (!userId || !scene) throw new Error("Missing userId or scene");
   const res = await fetch(`${BASE_URL}/progress/save`, {
